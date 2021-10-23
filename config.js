6c3712a1b7ca71baca7411978a9f5bb42e37cc70
Matthew Evans <m@iammatthew.com>
2021-10-22 21:18:01 -0700

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl' : 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '234190398695940',
+        clientSecret: 'bb166075961656f154b4292d53d7e50c'
+    }
 }
